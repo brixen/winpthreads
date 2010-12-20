ff93c9f4a166c3882daf09e3a0d19718f36b8c5b
ktietz70 <ktietz70@4407c894-4637-0410-b4f5-ada5f102cad1>
2010-12-20 15:33:39 +0000

    Remove unused code

--- src/cond.h
+++ src/cond.h
@@ -11,29 +11,6 @@
 #define LIFE_COND 0xC0BAB1FD
 #define DEAD_COND 0xC0DEADBF
 
-#if defined USE_COND_ConditionVariable
-#include "rwlock.h"
-#ifndef CONDITION_VARIABLE_INIT
-typedef struct _RTL_CONDITION_VARIABLE {                    
-        PVOID Ptr;                                       
-} RTL_CONDITION_VARIABLE, *PRTL_CONDITION_VARIABLE;      
-#define RTL_CONDITION_VARIABLE_INIT {0}                 
-#define RTL_CONDITION_VARIABLE_LOCKMODE_SHARED  0x1     
-
-#define CONDITION_VARIABLE_INIT RTL_CONDITION_VARIABLE_INIT
-#define CONDITION_VARIABLE_LOCKMODE_SHARED RTL_CONDITION_VARIABLE_LOCKMODE_SHARED
-
-typedef RTL_CONDITION_VARIABLE CONDITION_VARIABLE, *PCONDITION_VARIABLE;
-
-WINBASEAPI VOID WINAPI InitializeConditionVariable(PCONDITION_VARIABLE ConditionVariable);
-WINBASEAPI VOID WINAPI WakeConditionVariable(PCONDITION_VARIABLE ConditionVariable);
-WINBASEAPI VOID WINAPI WakeAllConditionVariable(PCONDITION_VARIABLE ConditionVariable);
-WINBASEAPI BOOL WINAPI SleepConditionVariableCS(PCONDITION_VARIABLE ConditionVariable,PCRITICAL_SECTION CriticalSection,DWORD dwMilliseconds);
-WINBASEAPI BOOL WINAPI SleepConditionVariableSRW(PCONDITION_VARIABLE ConditionVariable,PSRWLOCK SRWLock,DWORD dwMilliseconds,ULONG Flags);
-#endif /* CONDITION_VARIABLE_INIT */
-
-#endif /* USE_COND_ConditionVariable */
-
 #define STATIC_COND_INITIALIZER(x)		((pthread_cond_t)(x) == ((pthread_cond_t)PTHREAD_COND_INITIALIZER))
 
 typedef struct cond_t cond_t;
@@ -44,11 +21,6 @@ struct cond_t
     LONG waiters_count_; /* Number of waiting threads.  */
     LONG waiters_count_unblock_; /* Number of waiting threads whitch can be unblocked.  */
     LONG waiters_count_gone_; /* Number of waiters which are gone.  */
-#if defined USE_COND_ConditionVariable
-    CRITICAL_SECTION waiters_count_lock_; /* Serialize access to <waiters_count_>.  */
-    CONDITION_VARIABLE CV;
-
-#else /* USE_COND_Semaphore */
     CRITICAL_SECTION waiters_count_lock_; /* Serialize access to <waiters_count_>.  */
     CRITICAL_SECTION waiters_q_lock_; /* Serialize access to sema_q.  */
     LONG value_q;
@@ -58,7 +30,6 @@ struct cond_t
                  become signaled.  */
     HANDLE sema_b; /* Semaphore used to queue up threads waiting for the condition which
                  became signaled.  */
-#endif
 };
 
 void cond_print_set(int state, FILE *f);
